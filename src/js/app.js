a871f3d1c58b2cc4fef5950ab05b7524e8776d11
florisweb <floris@florisweb.dev>
2026-08-25 21:40:26 +0200

    Updated the planet generation method.

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,8 @@ const App = new class {
 
 
 const scene = new THREE.Scene();
-// scene.fog = new THREE.Fog( 0xffffff, 0, 150 );
+// scene.fog = new THREE.Fog( 0xaaaaaa, 0, 150);
+// scene.fog = new THREE.Fog( 0x333333, 0, 150);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 
 
@@ -68,20 +69,14 @@ Camera.position.z = 0;
 Camera.lookAt(0, 0, 0);
 
 
-
-// let light = new THREE.PointLight(0xffffff, 5000, 0, 2);
-// light.position.set(0, 0, 100);
-// scene.add(light);
-
-const sunDistance = 100;
-let sunLight = new THREE.PointLight(0xffffff, 10000, 0, 2);
+const sunDistance = 10000;
+let sunLight = new THREE.PointLight(0xffffff, sunDistance**2 * 1, 0, 2);
 sunLight.position.set(0, 0, sunDistance);
 scene.add(sunLight);
 window.sunLight = sunLight;
 
 // let ambientLight = new THREE.AmbientLight(0xffffff, .01);
 // scene.add(ambientLight);
-
 
 
 
@@ -140,14 +135,19 @@ function generatePLanetGeometry(radiusFunction, widthSegments = 32, heightSegmen
 
 
 function _perlin(_frequency) {
-	this.f = 1 / _frequency;
-	this.get = function(x, y) {
-		return Perlin.get((2 * x - 1) / this.f, (2 * y - 1) / this.f);
+	this.f = _frequency; 
+	this.get = function(theta, phi) { // Input: 0 - 2pi, 0 - pi
+		const u = theta / (2 * Math.PI);
+		const v = phi / Math.PI;
+
+		const compFreqU = this.f * Math.sin(phi)**0.2;
+		
+		return Perlin.get(u * compFreqU, v * this.f) * Math.abs(Math.sin(theta)) * Math.abs(Math.sin(phi));
 	}
 }
-let Perlin1 = new _perlin(6);
-let Perlin2 = new _perlin(12);
-let Perlin3 = new _perlin(30);
+let Perlin1 = new _perlin(5);
+let Perlin2 = new _perlin(15);
+let Perlin3 = new _perlin(50);
 window.Perlin1 = Perlin1;
 window.Perlin2 = Perlin2;
 window.Perlin3 = Perlin3;
@@ -155,25 +155,20 @@ window.Perlin3 = Perlin3;
 
 
 const segCount = 100;
-// const planetGeo = generatePLanetGeometry((theta, phi) => (1 + 0.3 * Math.random()) * 20, segCount * 2, segCount);
 const planetRad = 20;
-// const planetRadialFunc = (theta, phi) => (1 + 
-// 	Perlin1.get(theta / 2 / Math.PI, phi / Math.PI) * 0.2 + 
-// 	Perlin2.get(theta / 2 / Math.PI, phi / Math.PI) * 0.1 + 
-// 	Perlin3.get(theta / 2 / Math.PI, phi / Math.PI) * 0.02
-// 	) * planetRad;
-
-const planetRadialFunc = (theta, phi) => (1 + (
-	Perlin1.get(theta / 2 / Math.PI, phi / Math.PI) * 0.2 + 
-	Perlin2.get(theta / 2 / Math.PI, phi / Math.PI) * 0.1 + 
-	Perlin3.get(theta / 2 / Math.PI, phi / Math.PI) * 0.02) * 0.03
+const planetRadialFunc = (theta, phi) => (1 + 
+	Perlin1.get(theta, phi) * 0.1 + 
+	Perlin2.get(theta, phi) * 0.07 + 
+	Perlin3.get(theta, phi) * 0.03
 	) * planetRad;
+
 const planetGeo = generatePLanetGeometry(planetRadialFunc, segCount * 2, segCount);
 
-planetGeo.computeVertexNormals();
 
-// const planetGeo = new THREE.SphereGeometry( 30, 50, 50 );
 let material = new THREE.MeshLambertMaterial({color: 0xffffff});
+// let material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: false});
+// let material = new THREE.MeshNormalMaterial();
+
 let planetMesh = new THREE.Mesh(planetGeo, material);
 
 planetMesh.position.x = 0;
@@ -185,19 +180,16 @@ window.planetMesh = planetMesh;
 
 
 let sunAngle = 0;
+planetMesh.rotateX((Math.random() * 2 - 1) * Math.PI);
 
 function update() {
 	sunAngle += 0.01;
 	sunAngle = sunAngle % (2 * Math.PI);
 
 
-	// sunLight.position.set(Math.sin(sunAngle) * sunDistance, 0, Math.cos(sunAngle) * sunDistance);
+	sunLight.position.set(Math.sin(sunAngle) * sunDistance, 0, Math.cos(sunAngle) * sunDistance);
 
 	planetMesh.rotateY(0.01);
-
-
-
-	// sunLight.position.rotateZ
 
 
 	renderer.render(scene, Camera);
